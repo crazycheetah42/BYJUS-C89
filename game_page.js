0ff9458e76b9778345cb76c0292bada20bcaf0c6
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-02-04 19:34:37 -0500

    Update game_page.js and finish game

--- game_page.js
+++ game_page.js
@@ -1,14 +1,100 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + ": ";
+document.getElementById("player2_name").innerHTML = player2_name + ": ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+question_turn = "player1";
+answer_turn = "player2";
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+
 function send()
 {
-    number1 = document.getElementById("number1").value;
-    number2 = document.getElementById("number2").value;
-    actual_answer = parseInt(number1) * parseInt(number2);
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    
+    charAt1 = word.charAt(1);
 
-    question_number = "<h4>" + number1 + "x" + number2 + "</h4>";
+    length_divide_2 = Math.floor(word.length / 2);
+    charAt2 = word.charAt(length_divide_2);
+
+    length_minus_1 = word.length - 1;
+    charAt3 = word.charAt(length_minus_1);
+
+    remove_charAt1 = word.replace(charAt1, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
+
+    question_word = "<h4 id='word_display'>Q. " + remove_charAt3 + "</h4>";
     input_box = "<br>Answer: <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row = question_number + input_box + check_button;
+    row = question_word + input_box + check_button;
     document.getElementById("output").innerHTML = row;
-    document.getElementById("number1").value = "";
-    document.getElementById("number2").value = "";
+    document.getElementById("word").value = "";
+}
+
+function check()
+{
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("Answer in lower case: " + answer);
+    if (answer == word)
+    {
+        if (answer_turn == "player1")
+        {
+            player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else
+        {
+            player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+    if (question_turn == "player1")
+    {
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else 
+    {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+    if (answer_turn == "player1")
+    {
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+    else
+    {
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
+
+    player1_score_span = document.getElementById("player1_score").value;
+    player2_score_span = document.getElementById("player2_score").value;
+
+    if (player1_score_span == 10)
+    {
+        document.getElementById("the_h2_tag").innerHTML = player1_name + " wins!!!";
+        document.getElementById("player_question").innerHTML = "";
+        document.getElementById("player_answer").innerHTML = "";
+    }
+    else if (player2_score_span == 10)
+    {
+        document.getElementById("the_h2_tag").innerHTML = player2_name + " wins!!!";
+        document.getElementById("player_question").innerHTML = "";
+        document.getElementById("player_answer").innerHTML = "";
+    }
 }
